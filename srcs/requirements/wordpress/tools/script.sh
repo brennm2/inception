e30b0dc3d27ae52c8ee4ec16405a988c0b804849
brennm2 <brendon_marcello@yahoo.com>
2024-11-11 18:08:00 +0000

    final 2

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,6 +7,9 @@ cd /var/www/html
 # - Check if WordPress is already installed
 if [ -f "wp-config.php" ]; then
     echo -e "\e[34mWordPress is already installed.\e[0m"
+    echo -e "WPRESS_SECOND_USER=${WPRESS_SECOND_USER}"
+    echo -e "WPRESS_SECOND_EMAIL=${WPRESS_SECOND_EMAIL}"
+    echo -e "WPRESS_SECOND_PASSWORD=$(head -n 1 ${WPRESS_SECOND_PASSWORD})"
 else
     # - Install WordPress using WP-CLI
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -17,20 +20,22 @@ else
 
     ./wp-cli.phar config create --dbname="${DATABASE_NAME}" \
                                 --dbuser="${DATABASE_USER}" \
-                                --dbpass="${DATABASE_PASSWORD}" \
+                                --dbpass="$(head -n 1 ${DATABASE_PASSWORD})" \
                                 --dbhost="${DATABASE_HOST}" \
                                 --allow-root
 
     ./wp-cli.phar core install --url="${WPRESS_URL}" \
                                --title="${WPRESS_TITLE}" \
                                --admin_user="${WPRESS_ADMIN}" \
-                               --admin_password="${WPRESS_ADMIN_PASSWORD}" \
+                               --admin_password="$(head -n 1 ${WPRESS_ADMIN_PASSWORD})" \
                                --admin_email="${WPRESS_ADMIN_EMAIL}" \
                                --allow-root
+  
 
     ./wp-cli.phar user create ${WPRESS_SECOND_USER} \
-                              ${WPRESS_SECOND_EMAILWP_EMAIL} \
-                              --user_pass="${WPRESS_SECOND_PASSWORD}" \
+                              ${WPRESS_SECOND_EMAIL} \
+                              --role=contributor \
+                              --user_pass="$(head -n 1 ${WPRESS_SECOND_PASSWORD})" \
                               --allow-root 
 fi
 
